5a7bd57ff88cad516443c3d6063f73e91aae2baf
Mike Spaans <99069233@mydavinci.nl>
2022-03-16 10:43:33 +0100

    Lichknop.py afgemaakt

--- lightswitch.py
+++ lightswitch.py
@@ -1,8 +1,8 @@
 import tkinter as tk
 window = tk.Tk()
 
-# button = tk.Button(text='...', bg="white", fg="black")
-# button.pack(pady = 20, padx = 20)
+button = tk.Button(text='...', bg="white", fg="black")
+button.pack(pady = 20, padx = 20)
 
 # schijf hier tussen je code
 
@@ -11,32 +11,24 @@ KnopUit_Aan = True
 def KnopTekst():
     if button.config("text")[-1] == "switch light on":
         button.config(text="switch light off")
+        window.config(background='yellow')
+
         print ("light is on")
     else:
         button.config(text="switch light on")
+        window.config(background='black')
+
         print ("light is off")
 
-def KleurKnop():
-    if window.config("background")[-1] == "yellow":
-        window.config(background='black')
-    else:
-        window.config(background='yellow')
+
         
 
 
 
-# if KnopUit_Aan == True:
 button = tk.Button(text='switch Light off', command=KnopTekst)
 button.pack(pady = 20, padx = 20)
-window.config(background=KleurKnop())
-KnopUit_Aan = False
+window.config(background=KnopTekst())
 
-
-# elif KnopUit_Aan == False:
-#     button = tk.Button(text='Switch Light off', command=PrintUit_Aan)
-#     button.pack(pady = 20, padx = 20)
-#     window.configure(background='yellow')
-#     KnopUit_Aan = True
 
 
 # schijf hier tussen je code
